c7cffdac2c0841b1f9406773e400385794f0afb2
aishwarya-2708 <aishupatil2708@gmail.com>
2024-07-30 22:00:46 +0530

    Add files via upload
    This is submission of Task 5 (Simple Calculator using React.js)

--- App.js
+++ App.js
@@ -1,62 +1,60 @@
-// Aishwarya Ashok Patil 
+//Aishwarya Ashok Patil
 
-// import logo from './logo.svg';
-import './App.css';
 import {useState} from 'react';
-import deleteIcon from "./delete2.svg"
-import editIcon from "./edit2.svg"
+import './App.css';
 
-function App() {
-  const [input , setInput]=useState();
-  const [list , setList]=useState([]);
-  const [uid , setUid]=useState();
-  const [update , setUpdate]=useState(false);
-
-  const handleInput = (e)=>{
-    setInput(e.target.value)
-  }
-  const handleTask = ()=>{
-    setList([...list,input])
-    setInput("")
-    // setInput(e.target.value)
-  }
-  const handleDelete = (i)=>{
-   const filterList = list.filter((elm)=>elm !== list[i])
-    console.log("filterList",filterList);
-    setList(filterList)
-  }
-  const handleEdit = (i)=>{
-   const filterList = list.filter((elm)=>elm === list[i])
-    setInput(filterList[0])
-    setUid(i)
-    setUpdate(true)
-  }
-  const handleUpdate = ()=>{
-    list.splice(uid,1,input);
-    setInput("")
-    setUpdate(false)
-  }
+const App=() => {
+  const[value,setValue]=useState('');
   return (
     <div className="App">
-     <h1>TO-DO LIST</h1>
-     <div className="container">
-     <div className="inputBox">
-     <input type="text" value={input} onChange={(e)=>handleInput(e)} placeholder='Enter Task'/>
-     {update ? <button onClick={handleUpdate}>Update</button> : <button onClick={handleTask}>Add Task</button>
-    }
-      
-     </div>
-      <div className="list">
-        <ul>
-          {list.map((item,i)=><li key={i} >{item} <img src={deleteIcon} className='dlt-icon' alt="Delete" onClick={()=>handleDelete(i)}/>
-          <img src={editIcon} className='edit-icon' alt="Edit" onClick={()=>handleEdit(i)}/>
-          </li>)}
-        </ul>
-      </div>
-     </div>
-    <h4> &copy; Aishwarya Ashok Patil . </h4>
+        <div className="cont">
+          <div className="calculator">
+            <form action="">
+              <div className="display">
+                <input type="text" value={value} />
+
+              </div>
+
+              <div>
+                <input type="button" value="AC" onClick={e=>setValue('')} />
+                <input type="button" value="DE" onClick={e=>setValue(value.slice(0,-1))}/>
+                <input type="button" value="." onClick={e=>setValue(value+e.target.value)} />
+                <input type="button" value="/" onClick={e=>setValue(value+e.target.value)} />
+              </div>
+
+              <div>
+      <input type="button" value="7" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="8" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="9" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="*" onClick={e=>setValue(value+e.target.value)}/>
     </div>
-    
+
+    <div>
+      <input type="button" value="4" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="5" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="6" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="+" onClick={e=>setValue(value+e.target.value)}/>
+    </div>
+
+    <div>
+      <input type="button" value="1" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="2" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="3" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="-" onClick={e=>setValue(value+e.target.value)}/>
+    </div>
+
+    <div>
+      <input type="button" value="00" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="0" onClick={e=>setValue(value+e.target.value)}/>
+      <input type="button" value="=" className="equal" onClick={e=>setValue(eval(value))}/>
+    </div>
+
+            </form>
+          </div>
+          
+        </div>
+       <h4> &copy;Aishwarya Ashok Patil .</h4>
+    </div>
   );
 }
 
